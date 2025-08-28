09a07ead423a51ffffda87b21e1073d1324d761f
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2025-08-28 21:37:03 +0600

    frontend

---
+++ pos-frontend/src/components/home/RecentOrders.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RecentOrders = () => {
+  return (
+    <div className='px-8 mt-6'>
+        <div className='bg-[#1a1a1a] w-full h-[450px] rounded-lg'>
+            <div className='flex justify-between items-center px-6 py-4'>
+                <h1 className='text-[#f5f5f5] text-lg font-semibold tracking-wide'>Recent Order</h1>
+                <a herf="" className='text-[#025cca] text-sm font-semibold'>View All</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RecentOrders
