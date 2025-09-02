fd6b223b2b556e975fa657824aeb12b5e36f9180
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2025-09-02 20:38:23 +0600

    Order Modal UI Design

--- pos-frontend/src/components/shared/BottomNav.jsx
+++ pos-frontend/src/components/shared/BottomNav.jsx
@@ -1,13 +1,26 @@
-import React, { use } from 'react'
+import React, { useState } from 'react'
 import { FcHome } from "react-icons/fc";
 import { IoRestaurantSharp } from "react-icons/io5";
 import { MdTableBar } from "react-icons/md";
 import { CgMoreO } from "react-icons/cg";
 import { BiSolidDish } from "react-icons/bi";
 import { useNavigate } from 'react-router-dom';
+import Modal from './Modal';
 const BottomNav = () => {
 
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen ] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  const [guestCount, setGuestCount] = useState(0);
+
+  const increment = () => setGuestCount((prev)=> prev + 1 );
+
+
+  const decrement = () => {
+    if(guestCount <= 0 ) return;
+    setGuestCount((prev)=> prev - 1 );
+  }
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">   
     <button onClick={() => navigate("/")}className=" flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]"><FcHome  className="inline mr-2 size={15}"/> <p>Home</p></button>
@@ -15,7 +28,35 @@ const BottomNav = () => {
     <button onClick={() => navigate("/tables")}className=" flex items-center justify-center text-[#ababab] w-[200px] rounded-[20px]"><MdTableBar className="inline mr-2 size={15}"/> <p>Tables</p></button>
     <button className=" flex items-center justify-center text-[#ababab] w-[200px] rounded-[20px]"><CgMoreO  className="inline mr-2 size={15}"/> <p>More</p></button>
     
-    <button className=" absolute bottom-6 bg-[#f6b100] text-[#f5f5f5] rounded-full p-3 items-center"><BiSolidDish size={30}/></button>
+    <button onClick={openModal} 
+    className=" absolute bottom-6 bg-[#f6b100] text-[#f5f5f5] rounded-full p-3 items-center"><BiSolidDish size={30}/></button>
+    
+    <Modal isOpen={isModalOpen} onClose={closeModal} title="Create order">
+      <div >
+        <label className="block text-[#ababab] mb-2 text-sm font-medium">Customer Name</label>
+        <div className="flex items-center border border-[#333] rounded-[20px] p-2">
+          <input type="text" name="" placeholder="Enter customer name" id="" className="bg-transparent flex-1 text-white focus:outline-none"/>
+        </div>
+      </div>
+      <div >
+        <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">Phone Number</label>
+        <div className="flex items-center border border-[#333] rounded-[20px] p-2">
+          <input type="number" name="" placeholder="Enter customer number" id="" className="bg-transparent flex-1 text-white focus:outline-none"/>
+        </div>
+      </div>
+      <div>
+        <label className="block mb-2 mt-3 text-sm font-medium text-[#ababab]">Guest</label>
+        <div className="flex items-center justify-between bg-[#1f1f1f] px-4 py-3 rounded-lg">
+          <button onClick={decrement}  className="text-yellow-500 text-2xl">&minus;</button>
+          <span className="text-white">{guestCount} Person</span>
+          <button onClick={increment} className="text-yellow-500 text-2xl">&#43;</button>
+        </div>
+      </div>
+      <div>
+        <button onClick={()=> navigate("/tables")} className="w-full bg-[#F6B100] text-[#f5f5f5] rounded-lg py-3 mt-8
+        hover:bg-yellow-700">Create Order</button>
+      </div>
+    </Modal>
     </div>
     )
 }
